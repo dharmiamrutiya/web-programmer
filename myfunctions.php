a9637dd3a4902ca2d1c083889cf59598b8d7492b
dharmiamrutiya <155127114+dharmiamrutiya@users.noreply.github.com>
2024-01-11 09:04:47 +0530

    Add files via upload

---
+++ myfunctions.php
@@ -0,0 +1,81 @@
+<?php
+
+			#methematical function
+#$a=abs(-5);
+#echo $a;
+
+#$a=sqrt(16);
+#echo $a;
+
+#$a=rand(1,100);
+#echo $a;
+
+#$a=round(3.7);
+#echo $a;
+
+#$a=ceil(3.2);
+#echo $a;
+
+#$a=floor(3.8);
+#echo $a;
+
+#$a=pow(2,3);
+#echo $a;
+
+#$a=max(5,8,10);
+#echo $a;
+
+#$a=min(5,8,10);
+#echo $a;
+
+				#string function
+				
+#$a=strlen("hello");
+#echo $a;
+
+#$a=strpos("hello,world","world");
+#echo $a;
+
+#$a=str_replace("world","universe","hello world");
+#echo $a;
+
+#$a=substr("hello,world",0,5);
+#echo $a;
+
+#$a=strtolower("hello,world");
+#echo $a;
+
+#$a=strtoupper("hello,world");
+#echo $a;
+
+#$a=ucfirst("hello,world");
+#echo $a;
+
+#$a=strrev("hello,world");
+#echo $a;
+
+#$a=trim("hello,world");
+#echo $a;
+
+				# date & time function
+				
+#$a=date("Y-m-d H:i:s");
+#echo $a;
+
+#$a=time();
+#echo $a;
+
+#$a=strtotime("2023-08-01");
+#echo $a;
+
+#$a=gmdate("Y-m-d H:i:s");
+#echo $a;
+
+#$a= mktime(12,0,0,8,1,2023);
+#echo $a;
+
+#$a= date_default_timezone_set("america/new_york");
+#echo $a;
+
+			
+?>
