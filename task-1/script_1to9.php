f6f76587623c5405b5b1efa515c5306a891502ac
dharmiamrutiya <155127114+dharmiamrutiya@users.noreply.github.com>
2024-03-04 08:11:24 +0530

    Add files via upload

---
+++ task-1/script_1to9.php
@@ -0,0 +1,79 @@
+ 
+// script1.php
+
+<?php
+echo "hello world";
+?>
+ 
+// script2.php
+
+<?php
+$name= "Amrutiya Dharmi  ";
+$age= 20;
+echo "My name is ".$name."<br>";
+echo "  I am ".$age." years old";
+?>
+ 
+// script3.php
+
+<?php
+$num1=10;
+$num2=20;
+
+$sum=$num1+$num2;
+echo "sum of two numbers=".$sum;
+
+?>
+
+// script4.php
+
+<?php
+$a="hello";
+$b="php";
+echo $a." ".$b;
+?>
+ 
+// script5.php
+
+<?php
+$firstName="Dharmi";
+$lastName="Amrutiya";
+
+echo $firstName." ".$lastName;
+
+?>
+ 
+// script6.php
+
+<?php
+echo "<h3>information</h3>";
+echo "<b><i>bca</i></b><br>";
+echo "<b><i>b1</i></b>";
+?>
+ 
+// script7.php
+
+<?php
+$float1=5.5;
+$float2=10.5;
+echo "$float1<br>";
+echo "$float2";
+?>
+ 
+// script8.php
+
+<?php
+$name="dharmi";
+$age=20;
+$city="rajkot" 
+?>
+ 
+// script9.php
+
+<?php
+
+echo "<h1> information </h1><br>";
+echo "<b> name = Dharmi </b><br>";
+echo "<b> city = Rajkot </b>";
+
+?>
